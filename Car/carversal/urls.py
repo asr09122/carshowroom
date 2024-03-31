51ae71327b9595c104f9274c0b63fe9192b85d70
asr09122 <asrangpuri09@gmail.com>
2024-03-31 13:22:24 +0530

    login page working

--- Car/carversal/urls.py
+++ Car/carversal/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.index, name='Car'),
     path("search/", views.search, name='search'),
     path("login/",views.login,name='login'),
-    path("signup/",views.signup,name='name')
+    path("signup/",views.signup,name='signup')
     ]
--- Car/carversal/views.py
+++ Car/carversal/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
 from .models import Car
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.contrib import messages
 from django.urls import reverse
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate,logout
+from django.contrib.auth import login as auth_login
 User = get_user_model()
+
 def index(request):
     car = Car.objects.all()
     n = len(car)
@@ -44,6 +46,37 @@ def search(request):
     return render(request, "carversal/search.html", context)
 
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        fname = request.POST.get('first_name')
+        lname = request.POST.get('last_name')
+        password = request.POST.get('password')
+        cnfpwd = request.POST.get('confirm_password')
+
+        if password == cnfpwd:
+            # Create the user, authenticate, and login
+            myuser = User.objects.create_user(username, email, password)
+            myuser.first_name = fname
+            myuser.last_name = lname
+            myuser.save()
+
+            messages.success(request, "Your account has been successfully created")
+            return redirect(reverse('carversal:login'))
+        else:
+            messages.error(request, "Passwords do not match")
+            return redirect(reverse('carversal:signup'))
+
     return render(request, 'carversal/signup.html')
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            fname1 = User.first_name
+            return redirect(reverse('carversal:Car'), fame=fname1)  # Passing fame as a keyword argument
+
     return render(request, 'carversal/login.html')
